9e6b8837c1fd785a0165283eeb758ac819b3e721
Polina <polina@blsv.space>
2023-01-16 11:30:36 +0100

    the main page were adopted to be used as a react component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import spock from "./spock.jpg"
 
 function App() {
   return (
@@ -20,7 +21,47 @@ function App() {
     //   </header>
     // </div>
       <div className="App">
-        <header className="App-header"></header>
+        <header className="App-header" id="title">15 Times when Spock saved the Galaxy.</header>
+          <div id="img-div">
+              <img id="image" src={spock} alt='a picture of Spock'/>
+              <p id="img-caption">
+                An image of Spock looking at someone on the right side.
+              </p>
+          </div>
+        <section id="tribute-info">
+            <p id='intro'>
+                Not many people know this, but there is a brief list of what he achieved during his long and
+                profound life.
+            </p>
+            <ul id="tribute-list">
+                <li className="tribute-list_element">
+                    He stole the Infinity Gauntlet hence prevented the use of it.
+                </li>
+                <li className="tribute-list_element">
+                    2
+                </li>
+                <li className="tribute-list_element">
+                    3
+                </li>
+                <li className="tribute-list_element">
+                    4
+                </li>
+                <li className="tribute-lis_element">
+
+                </li>
+                <li className="tribute-lis_element">
+
+                </li>
+                <li className="tribute-lis_element">
+
+                </li>
+                <li className="tribute-lis_element">
+
+                </li>
+            </ul>
+            <a id="tribute-link" href="" target="_blank">See more here (the link is empty)</a>
+
+            </section>
       </div>
   );
 }
