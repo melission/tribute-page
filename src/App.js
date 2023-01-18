ebe97caaed7bc3d3c174b9f706611e9c5d5c636f
Polina <polina@blsv.space>
2023-01-18 10:09:45 +0100

    misprint corrections

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     // </div>
       <div className="App">
           <head>
-              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
               <meta charSet={"utf-8"} />
               <title>Spock tribute page</title>
           </head>
